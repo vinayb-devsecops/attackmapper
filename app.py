7754b853ab278b13ec3e907812a00952ba1c86f4
Vinay Butty <vinay.butty@tcpwave.com>
2026-06-12 20:07:30 +0530

    Add MITRE technique search capability

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
@@ -9,9 +9,19 @@ def home():
     with open("data/techniques.json","r") as f:
         techniques = json.load(f)
 
+    search = request.args.get("search","")
+
+    if search:
+        techniques = [
+            t for t in techniques
+            if search.lower() in t["technique"].lower()
+            or search.lower() in t["name"].lower()
+        ]
+
     return render_template(
         "index.html",
-        techniques=techniques
+        techniques=techniques,
+        search=search
     )
 
 @app.route("/api/techniques")
